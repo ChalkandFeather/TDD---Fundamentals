6c75ac387d59976286e6af3569159339bd1ece93
marie taylor <marietaylortailor@hotmail.com>
2023-03-21 21:05:39 +0000

    signal passes without triggering bandpass

--- plugin/lib/band_pass_filter.rb
+++ plugin/lib/band_pass_filter.rb
@@ -1,13 +1,8 @@
     def band_pass_filter(signal, lower_limit, upper_limit)
-      by_passed = []
-      
-      signal.each do |frequency| 
-         if frequency < lower_limit 
-            frequency.replace(lower_limit)
-            by_passed << frequency
+    
             return signal
           end
-        end
+      
     
 
 
