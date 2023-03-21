d2ff7caf507ab5a215a13f63122a26f671e6078a
marie taylor <marietaylortailor@hotmail.com>
2023-03-21 20:12:24 +0000

    Signal passes through filter, unlimimted.Freen test

--- plugin/lib/band_pass_filter.rb
+++ plugin/lib/band_pass_filter.rb
@@ -1,2 +1,3 @@
 def band_pass_filter(frequency, lower_limit, upper_limit)
+return frequency
 end
--- plugin/spec/band_pass_filter_spec.rb
+++ plugin/spec/band_pass_filter_spec.rb
@@ -1,7 +1,11 @@
 require 'band_pass_filter'
 
 RSpec.describe 'band pass filter' do
-  it 'checks for soundwave' do
+  it 'checks for occurence of soundwave' do
   expect(band_pass_filter(nil, nil, nil)).to eq(nil)
   end
+
+  it 'soundwave passes through filter unlimited' do
+    expect(band_pass_filter(25, nil, nil)).to eq(25)
+    end
 end
