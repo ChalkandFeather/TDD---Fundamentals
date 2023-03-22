5816fd4779b9ccbb83ae0e1465c69cf47c587a6e
marie taylor <marietaylortailor@hotmail.com>
2023-03-22 08:56:28 +0000

    high low limits method

--- plugin/lib/band_pass_filter.rb
+++ plugin/lib/band_pass_filter.rb
@@ -1,8 +1,21 @@
     def band_pass_filter(signal, lower_limit, upper_limit)
-    
-            return signal
-          end
-      
+      by_passed = []
+
+      signal.each do |frequency| 
+        if frequency < lower_limit
+          by_passed << lower_limit
+        elsif frequency > upper_limit
+          by_passed << upper_limit
+        else 
+          by_passed << frequency
+        end
+      end
+        return by_passed
+      end
+  
+  
+
+
     
 
 
--- plugin/spec/band_pass_filter_spec.rb
+++ plugin/spec/band_pass_filter_spec.rb
@@ -1,15 +1,24 @@
 require 'band_pass_filter'
 
 RSpec.describe 'band pass filter' do
-  it 'checks for occurence of soundwave' do
-  expect(band_pass_filter(nil, nil, nil)).to eq(nil)
-  end
 
-  it 'signal passes through filter unlimited' do
-    expect(band_pass_filter(25, nil, nil)).to eq(25)
+   it " signal passes and lower limit is triggered, raising  frequency to limit" do
+     expect(band_pass_filter([],nil ,nil )).to eq([])
+   end
+
+    it " signal passes and lower limit is triggered, raising  frequency to limit" do
+      expect(band_pass_filter([], 35, 110)).to eq([])
+    end
+
+    it " signal passes and lower limit is triggered, raising  frequency to limit" do
+    expect(band_pass_filter([50], 35, 110)).to eq([50])
     end
 
     it 'signal passes through filter without triggering filter limits' do
       expect(band_pass_filter([25, 30, 70], 10, 80)).to eq([25, 30, 70])
       end
-end
+
+      it " signal passes and lower limit is triggered, raising  frequency to limit" do
+        expect(band_pass_filter([30, 50, 100], 35, 110)).to eq([35, 50, 100])
+      end
+  end
