6de7a3286deb06c4d4d486d6f82bc96d662ab8cf
marie taylor <marietaylortailor@hotmail.com>
2023-03-24 20:02:51 +0000

    refactored with map method,removing need for empty array

--- plugin/lib/band_pass_filter.rb
+++ plugin/lib/band_pass_filter.rb
@@ -1,19 +1,42 @@
-    def band_pass_filter(signal, lower_limit = 40, upper_limit = 1000)
-     raise ArgumentError, "signal must be a non-empty array" if signal.empty?
+  def band_pass_filter(signal, lower_limit, upper_limit)
+    raise ArgumentError, "signal must be a non-empty array" if signal.empty?
+ 
+    by_passed = []
+  
+    signal.map do |frequency| 
+      if frequency < lower_limit
+        lower_limit
+      elsif frequency > upper_limit
+        upper_limit
+      else 
+        frequency
+      end
+    end
+  end
     
-     by_passed = []
+   
+  
+  
+  
+  
+  
+  
+    # def band_pass_filter(signal, lower_limit = 40, upper_limit = 1000)
+    #  raise ArgumentError, "signal must be a non-empty array" if signal.empty?
+    
+    #  by_passed = []
 
-      signal.each do |frequency| 
-        if frequency < lower_limit
-          by_passed << lower_limit
-        elsif frequency > upper_limit
-          by_passed << upper_limit
-        else 
-          by_passed << frequency
-        end
-      end
-        return by_passed
-      end
+      # signal.each do |frequency| 
+      #   if frequency < lower_limit
+      #     by_passed << lower_limit
+      #   elsif frequency > upper_limit
+      #     by_passed << upper_limit
+      #   else 
+      #     by_passed << frequency
+      #   end
+      # end
+      #   return by_passed
+      # end
   
   
 
