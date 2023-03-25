91b52561ec426ee9f5ee5bd93f53e802ad81053c
marie taylor <marietaylortailor@hotmail.com>
2023-03-25 17:07:10 +0000

    returns true if (n) is even with floating point set to zero

--- is_even/spec/is_even_spec.rb
+++ is_even/spec/is_even_spec.rb
@@ -2,25 +2,31 @@ require 'is_even'
 
 RSpec.describe 'is_even' do
     describe 'is_even' do
-    #  it 'returns nothing, if no arguement provided ' do
-    #     expect(is_even()).to eq()
-    #   end
-    # end
-
     it 'returns nil if no arguemnt provided' do
       expect(is_even()).to eq(nil)
       end
+    end
 
-    # it 'returns n if passed n' do
-    # expect(is_even(2)).to eq(2)
-    #   end
 
     it 'returns true if (n) is even' do
       expect(is_even(2)).to eq(true)
     end
-  end
-end
 
+    it 'returns true if (n) is even' do
+      expect(is_even(2)).to eq(true)
+    end
+
+    it 'returns true if (n) is even with floating point set to zero ' do
+      expect(is_even(2.0)).to eq(true)
+    end
+  end
+
+
+
+
+    # it 'returns n if passed n' do
+    # expect(is_even(2)).to eq(2)
+    #   end
 
 
     #  it 'returns true when (n) is even' do
