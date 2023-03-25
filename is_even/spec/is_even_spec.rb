bbbefdfbdff52b8b7ebcb67807f58b71800271df
marie taylor <marietaylortailor@hotmail.com>
2023-03-25 14:03:31 +0000

    returns nil when n is empty

--- is_even/spec/is_even_spec.rb
+++ is_even/spec/is_even_spec.rb
@@ -2,13 +2,18 @@ require 'is_even'
 
 RSpec.describe 'is_even' do
     describe 'is_even' do
-     it 'if no arguement given returns nothing' do
+     it 'returns nothing, if no arguement provided ' do
         expect(is_even()).to eq ()
       end
     end
 
+    it 'returns nil if no arguemnt provided' do
+      expect(is_even()).to eq(nil)
+      end
+end
 
-  end
+
+
     #  it 'returns true when (n) is even' do
     #     expect(is_even(2)).to eq true
     #   end
